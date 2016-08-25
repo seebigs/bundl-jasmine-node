896c915af385e2301d71ca935ffe0fd21e7c2b79
Chris Bigelow <seebigs@gmail.com>
2016-08-24 20:03:24 -0400

    CB: improved global environment

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ var jsdom = require("jsdom").jsdom;
 
 var reporter = require('./reporter.js');
 
-function testJasmine (b, files, options, callback) {
+
+function enhanceToString () {
     "use strict"; // causes fn.call(null) to return [object Null] instead of [object global]
 
-    var jasmine = new Jasmine();
-
-    var doc = jsdom('<!DOCTYPE html>', {});
-    global.window = doc.defaultView;
-    global.document = window.document;
-
-    global.navigator = window.navigator;
-
-    // Enhance toString output for DOM nodes
     var oldToString = Object.prototype.toString;
     Object.prototype.toString = function () {
         var ts = oldToString.call(this);
@@ -30,6 +22,54 @@ function testJasmine (b, files, options, callback) {
         }
         return ts;
     };
+}
+
+function testJasmine (b, files, options, callback) {
+
+    /* Init Global Env */
+
+    var win = jsdom('<!DOCTYPE html>', {}).defaultView;
+    win.document.hasFocus = function () { return true; };
+
+    // Adds LocalStorage
+    // This should only be needed until jsdom adds this feature
+    // https://github.com/tmpvar/jsdom/issues/1137
+    win.localStorage = win.sessionStorage = (function () {
+        return {
+            getItem: function (key) {
+                return this[key] || null;
+            },
+            setItem: function (key, value) {
+                this[key] = value;
+            },
+            removeItem: function (key) {
+                this[key] = null;
+            },
+        };
+    })();
+
+    global.window = win;
+
+    var preserve = [
+        'clearInterval',
+        'clearTimeout',
+        'setInterval',
+        'setTimeout',
+    ];
+
+    for (var x in win) {
+        if (preserve.indexOf(x) === -1) {
+            global[x] = win[x];
+        }
+    }
+
+    // Enhance toString output for DOM nodes
+    enhanceToString();
+
+
+    /* Init Jasmine */
+
+    var jasmine = new Jasmine();
 
     options.log = b.log;
     reporter.setReporterOptions(options);
